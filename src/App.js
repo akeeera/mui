4f02cc8f26448f8e202e0275b1747a42e841c0db
welldone <73180631+welldonemixtape@users.noreply.github.com>
2022-01-10 13:28:02 +0300

    изменена структура

--- src/App.js
+++ src/App.js
@@ -1,37 +1,25 @@
 import * as React from "react";
 import ReactDOM from "react-dom";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Button from "@mui/material/Button";
-import Link from "@mui/material/Link";
-import Typography from "@mui/material/Typography";
+import Posts from "./pages/Posts";
+import store from "./store/store.js";
+import { Provider } from "react-redux";
+import { createTheme, ThemeProvider, styled } from "@mui/material/styles";
+import { orange } from "@mui/material/colors";
+
+const theme = createTheme({
+  status: {
+    danger: orange[500],
+  },
+});
 
 function App() {
   return (
     <div className="App">
-      <Card sx={{ maxWidth: 345 }}>
-        <CardMedia
-          component="img"
-          height="140"
-          image="https://photocentra.ru/images/main46/463164_main.jpg"
-          alt="card-image"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Lizard
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button size="small"><Link href="#" underline="none">Share</Link></Button>
-          <Button size="small"><Link href="https://en.wikipedia.org/wiki/Lizard" underline="none">Learn More</Link></Button>
-        </CardActions>
-      </Card>
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <Posts />
+        </ThemeProvider>
+      </Provider>
     </div>
   );
 }
